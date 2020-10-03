33d74fcce062adc34706840811cabfce6e2a1ddb
taku.h72@gmail.com <kozue7269>
2020-10-03 17:45:39 +0900

    初期位置と初期高度を変更

--- js/Maps.js
+++ js/Maps.js
@@ -1,26 +1,29 @@
 "use strict";
 
 // Tell WorldWind to log only warnings and errors.
-WorldWind.Logger.setLoggingLevel(WorldWind.Logger.LEVEL_WARNING);
+WorldWind.Logger.setLoggingLevel(WorldWind.Logger.LEVEL_ERROE);
 
 // Create the WorldWindow.
 var wwd = new WorldWind.WorldWindow("canvasOne");
+wwd.navigator.lookAtLocation.latitude = 38;
+wwd.navigator.lookAtLocation.longitude = 140;
+wwd.navigator.range = 120000;
 
 // Create and add layers to the WorldWindow.
 var layers = [
     // Imagery layers.
     {layer: new WorldWind.BMNGLayer(), enabled: true},
-    {layer: new WorldWind.BMNGLandsatLayer(), enabled: false},
+    {layer: new WorldWind.BMNGLandsatLayer(), enabled: true},
     {layer: new WorldWind.BingAerialLayer(null), enabled: false},
-    {layer: new WorldWind.BingAerialWithLabelsLayer(null), enabled: true},
+    {layer: new WorldWind.BingAerialWithLabelsLayer(null), enabled: false},
     {layer: new WorldWind.BingRoadsLayer(null), enabled: false},
     {layer: new WorldWind.OpenStreetMapImageLayer(null), enabled: false},
     // Add atmosphere layer on top of all base layers.
     {layer: new WorldWind.AtmosphereLayer(), enabled: true},
     // WorldWindow UI layers.
-    {layer: new WorldWind.CompassLayer(), enabled: true},
+    {layer: new WorldWind.CompassLayer(), enabled: false},
     {layer: new WorldWind.CoordinatesDisplayLayer(wwd), enabled: true},
-    {layer: new WorldWind.ViewControlsLayer(wwd), enabled: true}
+    {layer: new WorldWind.ViewControlsLayer(wwd), enabled: false}
 ];
 
 for (var l = 0; l < layers.length; l++) {
